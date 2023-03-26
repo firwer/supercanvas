88432f6a49035a9717b4b36e0f8ba0b84c333963
firwer <firwer@gmail.com>
2023-03-26 16:52:06 +0800

    feat: quick files search on dashboard cards

---
+++ src/contentScript/contentScript.tsx
@@ -0,0 +1,241 @@
+import { addTask } from "../util/api";
+import SearchIcon from "@mui/icons-material/Search";
+import "./contentScript.css";
+import Dialog from "@mui/material/Dialog";
+import ReactDOM from "react-dom";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+
+import React, { useState } from "react";
+import {
+  Button,
+  CircularProgress,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  TextField,
+} from "@mui/material";
+const domain = window.location.origin;
+const current_page = window.location.pathname;
+// TODO: content script
+
+function loadQuickSearch() {
+  // Find the header element and append the search div to it
+  const headers = document.querySelectorAll(".ic-DashboardCard__header");
+  headers.forEach((header) => {
+    const newElement = makeElement("div", "search-btn", header, "Test");
+    const iconButton = <SearchIcon sx={{ width: "80px", height: "80px" }} />;
+    ReactDOM.render(iconButton, newElement);
+    header.appendChild(newElement);
+
+    // Add event listener to button click
+    newElement.addEventListener("click", function (e) {
+      const element = header.querySelector(".ic-DashboardCard__link");
+      const href = element.getAttribute("href");
+      const id = href.match(/\d+/)[0]; // extracts the first number in the href
+      console.log(id); // prints the ID number
+      const dialogContainer = document.createElement("div");
+      header.appendChild(dialogContainer);
+      ReactDOM.render(<DialogBox courseId={id} />, dialogContainer);
+    });
+  });
+}
+
+function DialogBox({ courseId }) {
+  const [open, setOpen] = React.useState(true);
+  const [files, setFiles] = React.useState([]);
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    async function fetchFiles() {
+      const response = await fetch(
+        `/api/v1/courses/${courseId}/files?per_page=100`
+      );
+      const data = await response.json();
+      setFiles(data);
+      setIsLoading(false);
+    }
+
+    fetchFiles();
+  }, [courseId]);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredFiles = files.filter((file) =>
+    file.display_name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  const handlePreviewClick = (id) => {
+    window.open(`https://canvas.nus.edu.sg/files/${id}`, "_blank");
+  };
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle sx={{ width: "500px", padding: "0px" }}>
+        <input
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+          type="text"
+          placeholder="Search Files..."
+          style={{
+            fontSize: "24px",
+            width: "100%",
+            height: "40px",
+            padding: "0.5rem",
+            border: "none",
+          }}
+        />
+      </DialogTitle>
+      <DialogContent
+        sx={{ alignItems: "center", justifyContent: "center", height: "500px" }}
+      >
+        {isLoading ? (
+          <CircularProgress />
+        ) : (
+          <ul style={{ padding: "0", margin: "0" }}>
+            {filteredFiles.length > 0 ? (
+              filteredFiles.map((file, index) => (
+                <li
+                  key={index}
+                  style={{
+                    listStyle: "none",
+                    borderBottom: "1px solid #ccc",
+                    padding: "10px",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <a
+                    href={file.url}
+                    style={{
+                      display: "block",
+                      textDecoration: "none",
+                      color: "#333",
+                    }}
+                    download
+                  >
+                    {file.display_name}
+                  </a>
+                  <IconButton
+                    onClick={() => handlePreviewClick(file.id)}
+                    aria-label="preview"
+                  >
+                    <RemoveRedEyeIcon />
+                  </IconButton>
+                </li>
+              ))
+            ) : (
+              <p>No files found</p>
+            )}
+          </ul>
+        )}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+function loadAssignmentButtons() {
+  try {
+    const row = document.getElementsByClassName("ig-row__layout");
+    console.log(row);
+    const TITLEREGEX = /Assignments:\s*(.*?)\s/;
+    const courseTitle = document
+      .querySelector("title")
+      .innerHTML.match(TITLEREGEX);
+    for (let i = 0; i < row.length; i++) {
+      // Element Selectors
+      const infoElement = row[i].querySelector(".ig-info");
+      const assignmentTitleElement =
+        infoElement.getElementsByClassName("ig-title");
+      const dateDueElement = infoElement.querySelector(
+        "span[data-html-tooltip-title]"
+      );
+      const newElement = createElement(
+        "div",
+        `<button title="Add to Microsoft To-Do List" class="btn">Add <img  width="20" height="20" src="${chrome.runtime.getURL(
+          "todo.png"
+        )}" alt="todo" class="btn-icon"></button>`
+      );
+
+      // Check if the button was clicked before
+      // console.log(assignmentTitleElement[0].innerHTML.trim());
+      // if (localStorage.getItem(assignmentTitleElement[0].innerHTML.trim())) {
+      //   newElement.querySelector(".btn").disabled = true;
+      //   newElement.querySelector(".btn").innerText = "Added";
+      // }
+      row[i].appendChild(newElement);
+
+      // Add event listener to button click
+      newElement.querySelector(".btn").addEventListener("click", function () {
+        const dueDate = dateDueElement
+          ? dateDueElement.getAttribute("data-html-tooltip-title")
+          : "";
+        const assignmentTitle = assignmentTitleElement[0].innerHTML.trim();
+        const moduleCode = courseTitle[1];
+        if (addTask(dueDate, assignmentTitle, moduleCode)) {
+          this.disabled = true;
+          this.innerText = "Added";
+          // chrome.storage.local.set({ assignmentTitle });
+        } else {
+          alert("Failed to add task");
+        }
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+window.addEventListener("load", function () {
+  console.log("Running");
+  loadQuickSearch();
+  const elementExists = document.querySelector(".ig-row__layout") !== null;
+  if (elementExists) {
+    loadAssignmentButtons();
+    const observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        if (mutation.type === "childList") {
+          setTimeout(() => {
+            if (document.querySelector(".btn")) {
+              console.log("button already exists, disconnecting observer");
+              observer.disconnect();
+            } else {
+              console.log("Couldn't find buttons, adding them");
+              loadAssignmentButtons();
+            }
+          }, 1000);
+        }
+      });
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  }
+});
+
+function createElement(type, html) {
+  const newElement = document.createElement(type);
+  newElement.innerHTML = html;
+  return newElement;
+}
+
+function makeElement(element, elclass, location, text) {
+  let creation = document.createElement(element);
+  creation.classList.add(elclass);
+  creation.textContent = text;
+  location.appendChild(creation);
+  return creation;
+}
